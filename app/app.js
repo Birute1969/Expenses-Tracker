620406faa42883f6e0e47727cc2e06f697cfbd94
Birute1969 <107874553+Birute1969@users.noreply.github.com>
2023-01-05 00:32:47 +0200

    App GET

--- app/app.js
+++ app/app.js
@@ -17,12 +17,43 @@ const mysqlConfig = {
 
 const connection = mysql.createConnection(mysqlConfig);
 
-app.get('/expenses', (req, res) => {
-    connection.execute('SELECT * FROM expenses', (err, expenses) => {
-        console.log(expenses);
-        res.send(expenses);
-    })
-})
+//Endpoint grąžina visus expenses (išlaidas):
+//   app.get('/expenses', (req, res) => {
+//       connection.execute('SELECT * FROM expenses', (err, expenses) => {
+//           console.log(expenses);
+//           res.send(expenses);
+//       })
+//   })
 
-const PORT = 3000;
+ // Endpointas grąžina išlaidas pagal išlaidų id:
+//Patestuokime endpoint per Postman
+
+ app.get('/expenses/:id', (req, res) => {
+      const {id} = req.params;
+      connection.execute('SELECT * FROM expenses WHERE id=?', [id], (err, expenses) => {
+          console.log(expenses);
+          res.send(expenses);
+      })
+  })
+
+// Endpointas grąžina būtent mūsų išlaidas pagal userId:
+
+// app.get('/expenses/:userId', (req, res) => {
+//     const { userId } = req.params;
+//     connection.execute('SELECT * FROM expenses WHERE userId=?', [userId], (err, expenses) => {
+//         console.log(expenses);
+//         res.send(expenses);
+//     })
+// })
+
+//Galima užrašyti kitu būdu:
+app.get('/expenses', (req, res) => {
+     const { userId } = req.query;
+     connection.execute('SELECT * FROM expenses WHERE userId=?', [userId], (err, expenses) => {
+         console.log(expenses);
+         res.send(expenses);
+     });
+});
+
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
